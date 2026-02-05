5323307225a37865eab37c197c6427cf9308c0dc
Michael Crapse <mikeeybikeey@gmail.com>
2026-02-05 15:42:57 -0800

    Add initial setup routes

--- stockup/urls.py
+++ stockup/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("stocks/", include("stocks.urls")),
+    path("", include("stocks.urls")),
     path("admin/", admin.site.urls),
 ]

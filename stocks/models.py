6e6676d827c1a401a23140b83295933c70b3f26b
Michael Crapse <mikeeybikeey@gmail.com>
2026-02-26 11:24:10 -0800

    Update models

--- stocks/models.py
+++ stocks/models.py
@@ -1,24 +1,54 @@
-import datetime
 
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import AbstractUser
 
 
-class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField("date published")
-    
-    def __str__(self):
-        return self.question_text
+# Django has built-in User model
+# Will need to redo the whole database if we switch to custom:
+#   https://docs.djangoproject.com/en/6.0/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
+#   https://docs.djangoproject.com/en/6.0/ref/settings/#auth-user-model
+#   https://docs.djangoproject.com/en/6.0/ref/contrib/auth/
+class User(AbstractUser):
+    # username from `AbstractUser`
+    # password from `AbstractUser`
+    # email from `AbstractUser`
+    balance = models.IntegerField() # balance in cents
 
 
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+class Stock(models.Model):
+    shares = models.IntegerField()
+    ticker = models.CharField(max_length=5)
+    value = models.IntegerField() # value in cents
 
-    def __str__(self):
-        return self.choice_text
-    
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Transaction(models.Model):
+    date = models.DateTimeField()
+    amount = models.IntegerField() # percent of product purchased
+    ticker = models.CharField(max_length=5)
+
+
+# must this be separate from `Transaction`?
+class TransactionHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transaction = models.ForeignKey(Transaction, on_delete=models.CASCADE)
+
+
+# Maps users to stocks they own.
+class Owns(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=50)
+    creation_date = models.DateField()
+    balance_per_capita = models.IntegerField()
+
+
+# History of a stock.
+# This is used to cach information from other APIs to avoid spamming them.
+class StockHistory(models.Model):
+    day = models.IntegerField()
+    date = models.DateField()
+    predicted_price = models.IntegerField() # value in cents
+    ticker = models.CharField(max_length=5)
